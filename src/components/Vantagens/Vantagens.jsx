2d6541de9529b436a45ef01a889a064e88ce4990
MachadONLY <rm551570@fiap.com.br>
2023-10-31 16:06:55 -0300

    mudei aq

--- src/components/Vantagens/Vantagens.jsx
+++ src/components/Vantagens/Vantagens.jsx
@@ -1,65 +1,24 @@
 import "./Vantagens.css"
-import icon from "../../assets/icon1.svg";
-import icon2 from "../../assets/icon2.svg";
-import icon3 from "../../assets/icon3.svg";
-import icon4 from "../../assets/icon4.svg";
-import icon5 from "../../assets/icon5.svg";
-import icon6 from "../../assets/icon6.svg";
+import safetyimg from "../../assets/safetyimg.svg"
+import safetyicon from "../../assets/safetyicon.svg"
 
 export default function Vantagens () {
     return(
-        <section className="third-section">
-        <div className="text-center">
-          <h1>Vantagens</h1>
-          <p>
-            Variedade de produtos para atender às suas necessidades. Visite-nos
-            para saber mais sobre nossas soluções de energia.
-          </p>
-        </div>
-
-        <div className="third-content">
-          <div className="big-square">
-            <div className="small-square">
-              <img src={icon} alt="" />
-              <h5>Descarbonização</h5>
-            </div>
+        <section className="content">
+          <h1><span className="green">Nossos</span> <span>Serviços</span></h1>
+          <div className="sub-text">
+            <p>Variedade de produtos para atender às suas necessidades. Visite-nos <br /> para saber mais sobre nossas soluções de energia.</p>
           </div>
 
-          <div className="big-square">
-            <div className="small-square">
-              <img src={icon2} alt="" />
-              <h5>Digitalização</h5>
+          <div className="cartinha">
+            <img src={safetyimg} alt="imagem energia solar" className="img" />
+            <div className="square">
+              <img src={safetyicon} alt="icon segurança" className="icon" />
             </div>
+              <h2>Segurança</h2>
+              <p>Nossa empresa prioriza a segurança em cada projeto de eficiência energética, protegendo tanto o ambiente quanto nossos clientes</p>
           </div>
-
-          <div className="big-square">
-            <div className="small-square">
-              <img src={icon3} alt="" />
-              <h5>Redução de Custo</h5>
-            </div>
-          </div>
-
-          <div className="big-square">
-            <div className="small-square">
-            <img src={icon4} alt="" />
-              <h5>Confiabilidade</h5>
-            </div>
-          </div>
-
-          <div className="big-square">
-            <div className="small-square">
-            <img src={icon5} alt="" />
-              <h5>Descentralização</h5>
-            </div>
-          </div>
-
-          <div className="big-square">
-            <div className="small-square">
-            <img src={icon6} alt="" />
-              <h5>Segurança</h5>
-            </div>
-          </div>
-        </div>
+        
       </section>
     )
 }
