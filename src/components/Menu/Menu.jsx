9382580f9f5565c6f440f430fa47fd08a2726700
MachadONLY <rm551570@fiap.com.br>
2023-11-11 09:57:09 -0300

    .

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,39 +6,39 @@ export default function Menu() {
 
   return (
     <div className="menu">
-  <div className="logo">
-    <img src={Logo} alt="Logo" />
-  </div>
-  <ul className= "menu-list" >
-    <li>
-      <Link to="/" className="menu-item">
-        Home
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <ul className= "menu-list" >
+        <li>
+          <Link to="/" className="menu-item">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/vantagens" className="menu-item">
+            Vantagens
+          </Link>
+        </li>
+        <li>
+          <Link to="/servicos" className="menu-item">
+            Serviços
+          </Link>
+        </li>
+        <li>
+          <Link to="/sobre" className="menu-item">
+            Sobre Nós
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="menu-item">
+            Login
+          </Link>
+        </li>
+      </ul>
+      <Link to="/fale" className="fale-conosco-button">
+        Fale Conosco
       </Link>
-    </li>
-    <li>
-      <Link to="/vantagens" className="menu-item">
-        Vantagens
-      </Link>
-    </li>
-    <li>
-      <Link to="/servicos" className="menu-item">
-        Serviços
-      </Link>
-    </li>
-    <li>
-      <Link to="/sobre" className="menu-item">
-        Sobre Nós
-      </Link>
-    </li>
-    <li>
-      <Link to="/login" className="menu-item">
-        Login
-      </Link>
-    </li>
-  </ul>
-  <Link to="/fale" className="fale-conosco-button">
-    Fale Conosco
-  </Link>
-</div>
+    </div>
   );
 }
