938f3982937fc61a81b4b75bf6f2c29d1552b06f
MachadONLY <rm551570@fiap.com.br>
2023-11-12 23:12:06 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import styles from "./scss/App.module.scss";
 import { Outlet } from "react-router-dom";
 import Nav from './components/Menu/Menu'
 import Footer from './components/Footer/Footer'
+import GlobalStyle from "../GlobalStyles";
 
 export default function App()  {
   return (
     <div>
+      <GlobalStyle/>
         <Nav/>
           <Outlet/>
         <Footer/>
-
     </div>
   )
 }
